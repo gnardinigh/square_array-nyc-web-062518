e9b750bb422566436b677ba732fe99ef34e7c237
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 20:49:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,4 @@
 def square_array(array)
   
-  new_array = Array.new
-  index = 0
-  
-  array.each do |element|
-    new_array[index]=element*element
-    index++
-  end
-  return new_array
-  end
   
   
